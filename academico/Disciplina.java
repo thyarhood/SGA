504e1d34b9e0a09658c9cab27b57797ce481a38d
Br_E_nin <102299699+thyarhood@users.noreply.github.com>
2023-06-28 00:07:22 -0300

    Update Disciplina.java

--- academico/Disciplina.java
+++ academico/Disciplina.java
@@ -74,7 +74,7 @@ private ArrayList<Integer>frequencia;
  }
  
  public void setProfessor(Professor professor) {
-	 this.professor = professor;
+  this.professor = professor;
  }
  
  public void removerProfessor() {
@@ -131,11 +131,11 @@ private ArrayList<Integer>frequencia;
  }
  
  public int getNumeroAlunos() {
-	 return alunos.size() - 1;
+  return alunos.size() - 1;
  }
 
  public double getFrequenciaAluno(int posAluno) {
-	 return frequencia.get(posAluno);
+  return frequencia.get(posAluno);
  }
  
  public void exibirReprovados() {
@@ -143,7 +143,6 @@ private ArrayList<Integer>frequencia;
   if(alunos.size() != 0) {
    for(int i = 0; i <= alunos.size() - 1; i++) {
     if(notas.get(i) <= 6 && frequencia.get(i) <= 75) {
-     System.out.println(i);
      System.out.println("O aluno " + alunos.get(i).getNome() + " cujo o RA é " + alunos.get(i).getRa() + " foi reprovado");
     }
     else {
@@ -157,5 +156,121 @@ private ArrayList<Integer>frequencia;
   else {
   System.out.println("Disciplinas não possui alunos matriculados");
   }
- }    
+ } 
+ 
+ public void exibirProfessor() {
+  if(this.professor != null) {
+  System.out.println(this.professor.getNome());
+  System.out.println(this.professor.getSuap());
+  }
+  else {
+   System.out.println("Essa disciplina nao possui professor ministrante");
+  }
+ }
+ 
+ public double getAproveitamentoAluno(int posAluno) {
+  int a = (frequencia.get(posAluno) * 100)/this.numTotalAulas;
+  return a;
+ }
+ 
+ public void desmatricularAlunos() {
+  for(int i = 0; i <= alunos.size() - 1; i++) {
+   this.alunos.remove(i);
+   this.frequencia.remove(i);
+   this.notas.remove(i);
+  }
+ }
+ 
+ public void exibirAlunosAcimaDaMedia() {
+  int a = 0;
+  if(alunos.size() != 0) {
+   for(int i = 0; i <= alunos.size() - 1; i++) {
+    if(notas.get(i) >= 6) {
+     System.out.println("O aluno " + alunos.get(i).getNome() + " cujo o RA é " + alunos.get(i).getRa() + " esta acima da media");
+     a++;
+    }
+   }
+   if(a == 0) {
+    System.out.println("Nenhum aluno esta acima da media");
+   }
+  }
+  else {
+   System.out.println("Essa disciplina nao possui alunos matriculados");
+  }
+ }
+ 
+ public void exibirAlunosAbaixoDaMedia() {
+  int a = 0;
+  if(alunos.size() != 0) {
+   for(int i = 0; i <= alunos.size() - 1; i++) {
+    if(notas.get(i) <= 6) {
+     System.out.println("O aluno " + alunos.get(i).getNome() + " cujo o RA é " + alunos.get(i).getRa() + " esta abaixo da media");
+     a++;
+    }
+   }
+   if(a == 0) {
+    System.out.println("Nenhum aluno esta abaixo da media");
+   }
+  }
+  else {
+   System.out.println("Essa disciplina nao possui alunos matriculados");
+  }
+ }
+ 
+ public void addNota(int posAluno, double nota) {
+  notas.set(posAluno, nota);
+ }
+ 
+ public double[] ordenaNotas() {
+  int contador=0,contadorMaior=0;
+  double n=0, m=0;
+  double num[] = new double[notas.size()];
+  
+  for(int i = 0; i <= notas.size() - 1; i++) {
+   num[i] = notas.get(i);
+   if(i > 0) {
+    for(int a = i - 1; a >= 0; a-- ) {
+     if(num[a] <= num[i]) {
+      contadorMaior++;
+     }
+     else {
+      continue;
+     }
+    }
+     contador = 0;
+     if(contadorMaior > 0) {
+      for(int b = i - contadorMaior; b <= i; b++ ) {
+       if(contador == 0) {
+        m = num[b];
+        num[i - contadorMaior] = num[i];
+        num[i] = m;
+        contador++;
+       }
+       else if(contador%2 == 0 && contador != 0) {
+        m = num[b];
+        num[b] = n;
+        contador++;
+       }
+       else if(contador%2 != 0){
+        n = num[b];
+        num[b] = m;
+        contador++;
+       }
+      }
+      contadorMaior = 0;   
+     }
+   }
+  } 
+   return num;
+ }
+ 
+ public void exibeOrdemNotas() {
+  double a[] = ordenaNotas();
+  for(int i = 0; i <= notas.size() - 1; i++) {
+   System.out.println(a[i]);
+  }
+ }
 }
+
+
+
